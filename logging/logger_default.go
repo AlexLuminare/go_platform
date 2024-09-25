4d856614b240189b9cb2a42132a0b4f3310b3a69
Alex Yersh <yersh.alex@gmail.com>
2024-09-25 11:50:34 +0300

    Добавлена документация для пакета logging

--- logging/logger_default.go
+++ logging/logger_default.go
@@ -1,3 +1,9 @@
+// Package logging предоставляет интерфейс для логирования и его стандартную реализацию.
+//
+// Этот пакет позволяет создавать логгеры с различными уровнями логирования,
+// такими как Trace, Debug, Information, Warning и Fatal. Он также предоставляет
+// возможность настройки минимального уровня логирования и поведения при фатальных ошибках.
+
 package logging
 
 import (
@@ -5,53 +11,70 @@ import (
 	"log"
 )
 
+// DefaultLogger представляет собой стандартную реализацию интерфейса Logger.
+//
+// Он поддерживает несколько уровней логирования и позволяет настраивать
+// минимальный уровень для вывода сообщений. Также имеет возможность
+// вызывать панику при логировании фатальных ошибок.
 type DefaultLogger struct {
 	minLevel     LogLevel
 	loggers      map[LogLevel](*log.Logger)
 	triggerPanic bool
 }
 
+// MinLogLevel возвращает текущий минимальный уровень логирования.
 func (l *DefaultLogger) MinLogLevel() LogLevel {
 	return l.minLevel
 }
 
+// write записывает сообщение в лог, если его уровень не ниже минимального.
 func (l *DefaultLogger) write(level LogLevel, message string) {
 	if l.minLevel <= level {
 		l.loggers[level].Output(2, message)
 	}
 }
 
+// Trace записывает сообщение с уровнем Trace.
 func (l *DefaultLogger) Trace(msg string) {
 	l.write(Trace, msg)
 }
 
+// Tracef записывает форматированное сообщение с уровнем Trace.
 func (l *DefaultLogger) Tracef(template string, vals ...interface{}) {
 	l.write(Trace, fmt.Sprintf(template, vals...))
 }
 
+// Debug записывает сообщение с уровнем Debug.
 func (l *DefaultLogger) Debug(msg string) {
 	l.write(Debug, msg)
 }
 
+// Debugf записывает форматированное сообщение с уровнем Debug.
 func (l *DefaultLogger) Debugf(template string, vals ...interface{}) {
 	l.write(Debug, fmt.Sprintf(template, vals...))
 }
 
+// Info записывает сообщение с уровнем Information.
 func (l *DefaultLogger) Info(msg string) {
 	l.write(Information, msg)
 }
 
+// Infof записывает форматированное сообщение с уровнем Information.
 func (l *DefaultLogger) Infof(template string, vals ...interface{}) {
 	l.write(Information, fmt.Sprintf(template, vals...))
 }
 
+// Warn записывает сообщение с уровнем Warning.
 func (l *DefaultLogger) Warn(msg string) {
 	l.write(Warning, msg)
 }
+
+// Warnf записывает форматированное сообщение с уровнем Warning.
 func (l *DefaultLogger) Warnf(template string, vals ...interface{}) {
 	l.write(Warning, fmt.Sprintf(template, vals...))
 }
 
+// Panic записывает сообщение с уровнем Fatal и вызывает панику, если это настроено.
 func (l *DefaultLogger) Panic(msg string) {
 	l.write(Fatal, msg)
 	if l.triggerPanic {
@@ -59,6 +82,7 @@ func (l *DefaultLogger) Panic(msg string) {
 	}
 }
 
+// Panicf записывает форматированное сообщение с уровнем Fatal и вызывает панику, если это настроено.
 func (l *DefaultLogger) Panicf(template string, vals ...interface{}) {
 	formattedMsg := fmt.Sprintf(template, vals...)
 	l.write(Fatal, formattedMsg)
