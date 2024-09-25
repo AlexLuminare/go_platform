a497f543826ae20e7149060ad75effc7e9244eaf
Alex Yersh <yersh.alex@gmail.com>
2024-09-25 13:51:34 +0300

    Изменения в проекте:
    1. Перемещение файла конфигурации:
       - Файл config/config.json перемещен в корневую директорию проекта и переименован в config.json.
       - Это упрощает доступ к конфигурационному файлу и делает его более заметным.
    
    2. Модификация logging/default_create.go:
       - изменена логика создания логгера или обработки уровней логирования.
    
    3. Изменения в main.go:
       - добавлены новые функции или изменена логика работы с логгером.

--- logging/default_create.go
+++ logging/default_create.go
@@ -7,23 +7,36 @@
 package logging
 
 import (
+	"go_platform/config"
 	"log"
 	"os"
+	"strings"
 )
 
-// NewDefaultLogger создает и возвращает новый экземпляр DefaultLogger с указанным минимальным уровнем логирования.
+// NewDefaultLogger создает и возвращает новый экземпляр DefaultLogger на основе предоставленной конфигурации.
 //
 // Параметры:
-//   - level: минимальный уровень логирования для нового логгера.
+//   - cfg: объект Configuration, содержащий настройки для логгера.
 //
 // Возвращает:
 //   - Logger: интерфейс Logger, реализованный структурой DefaultLogger.
 //
-// Созданный логгер использует стандартный вывод (os.Stdout) для всех уровней логирования.
-// Он включает временную метку в выводе лога и использует различные префиксы для каждого уровня логирования.
-// По умолчанию, логгер вызывает панику для логов уровня Fatal.
-func NewDefaultLogger(level LogLevel) Logger {
+// Функция выполняет следующие действия:
+// 1. Устанавливает флаги для форматирования вывода лога (временная метка и префикс сообщения).
+// 2. Определяет минимальный уровень логирования из конфигурации или использует Debug по умолчанию.
+// 3. Создает логгеры для каждого уровня логирования, используя стандартный вывод (os.Stdout).
+// 4. Настраивает логгер на вызов паники для сообщений уровня Fatal.
+//
+// Созданный логгер использует различные префиксы для каждого уровня логирования (TRACE, DEBUG, INFO, WARN, FATAL).
+func NewDefaultLogger(cfg config.Configuration) Logger {
 	flags := log.Lmsgprefix | log.Ltime
+
+	var level LogLevel = Debug
+	if configLevelString, found :=
+		cfg.GetString("logging:level"); found {
+		level = LogLevelFromString(configLevelString)
+	}
+
 	return &DefaultLogger{
 		minLevel: level,
 		loggers: map[LogLevel](*log.Logger){
@@ -36,4 +49,41 @@ func NewDefaultLogger(level LogLevel) Logger {
 
 		triggerPanic: true,
 	}
+}
+
+// LogLevelFromString преобразует строковое представление уровня логирования в соответствующее значение LogLevel.
+//
+// Параметры:
+//   - val: строка, представляющая уровень логирования.
+//
+// Возвращает:
+//   - level: соответствующее значение LogLevel.
+//
+// Функция нечувствительна к регистру и поддерживает следующие значения:
+//   - "debug" -> Debug
+//   - "information" -> Information
+//   - "warning" -> Warning
+//   - "fatal" -> Fatal
+//   - "none" -> None
+//
+// Если входная строка не соответствует ни одному из известных уровней,
+// функция возвращает уровень Debug по умолчанию.
+
+func LogLevelFromString(val string) (level LogLevel) {
+	switch strings.ToLower(val) {
+	case "debug":
+		level = Debug
+	case "information":
+		level = Information
+	case "warning":
+
+		level = Warning
+	case "fatal":
+		level = Fatal
+	case "none":
+		level = None
+	default:
+		level = Debug
+	}
+	return
 }
--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"go_platform/config"
+	"go_platform/logging"
 )
 
-func writeMessage() {
-	fmt.Println("Hello, Platform")
+func writeMessage(logger logging.Logger, cfg config.Configuration) {
+	section, ok := cfg.GetSection("main")
+	if ok {
+		message, ok := section.GetString("message")
+		if ok {
+			logger.Info(message)
+		} else {
+			logger.Panic("Cannot find configuration setting")
+		}
+	} else {
+		logger.Panic("Config section not found")
+	}
 }
+
 func main() {
-	writeMessage()
+	var cfg config.Configuration
+	var err error
+	cfg, err = config.Load("config.json")
+	if err != nil {
+		panic(err)
+	}
+	var logger logging.Logger = logging.NewDefaultLogger(cfg)
+	writeMessage(logger, cfg)
 }
